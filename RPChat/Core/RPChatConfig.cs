e37f8ffd50a9347e27f52907cd4a8a23cb6f93ae
AuD1t3k <uriybulakh12@gmail.com>
2020-11-25 19:53:26 +0200

    1.0

---
+++ RPChat/Core/RPChatConfig.cs
@@ -0,0 +1,104 @@
+﻿using Rocket.API;
+
+namespace RPChat.Core
+{
+    public class RPChatConfig : IRocketPluginConfiguration
+    {
+        public bool DisableGlobalChat;
+        public bool DisableLocalChat;
+        public bool DisableGroupChat;
+
+
+
+        public string   NRPChatColor;
+        public bool     NRPChatUseRichTextFormatting;
+
+        public string   SMSChatColor;
+        public bool     SMSChatUseRichTextFormating;
+        public ushort   SMSChatItemId;
+        public uint     SMSChatCost;
+        public string   SMSChatIcon;
+
+        public string   ADChatColor;
+        public bool     ADChatUseRichTextFormating;
+        public ushort   ADChatItemId;
+        public uint     ADChatCost;
+        public string   ADChatIcon;
+        public float    ADChatRadius;
+
+        public string   DNChatColor;
+        public bool     DNChatUseRichTextFormating;
+        public ushort   DNChatItemId;
+        public uint     DNChatCost;
+        public string   DNChatIcon;
+        public float    DNChatRadius;
+
+
+        public string   MEChatColor;
+        public float    MEChatRadius;
+        public bool     MEChatUseRichTextFormating;
+
+        public string   SChatColor;
+        public float    SChatRadius;
+        public bool     SChatUseRichTextFormating;
+
+        public string   WChatColor;
+        public float    WChatRadius;
+        public bool     WChatUseRichTextFormating;
+
+        public string   TRYChatColor;
+        public float    TRYChatRadius;
+        public bool     TRYChatUseRichTextFormating;
+
+
+        public void LoadDefaults()
+        {
+            DisableGlobalChat   = true;
+            DisableLocalChat    = true;
+            DisableGroupChat    = true;
+
+
+            NRPChatColor                    = "";
+            NRPChatUseRichTextFormatting    = true;
+
+            SMSChatColor                = "";
+            SMSChatUseRichTextFormating = true;
+            SMSChatItemId               = 0;
+            SMSChatCost                 = 0;
+            SMSChatIcon                 = "";
+
+            ADChatColor                 = "";
+            ADChatUseRichTextFormating  = true;
+            ADChatItemId                = 0;
+            ADChatCost                  = 0;
+            ADChatIcon                  = "";
+            ADChatRadius                = 0;
+
+
+            DNChatColor                 = "";
+            DNChatUseRichTextFormating  = true;
+            DNChatItemId                = 0;
+            DNChatCost                  = 0;
+            DNChatIcon                  = "";
+            DNChatRadius                = 0;
+
+
+            MEChatColor                 = "";
+            MEChatRadius                = 0;
+            MEChatUseRichTextFormating  = true;
+
+            SChatColor                  = "";
+            SChatRadius                 = 0;
+            SChatUseRichTextFormating   = true;
+
+            WChatColor                  = "";
+            WChatRadius                 = 0;
+            WChatUseRichTextFormating   = true;
+
+            TRYChatColor                = "";
+            TRYChatRadius               = 0;
+            TRYChatUseRichTextFormating = true;
+
+        }
+    }
+}
---
+++ RPChat/Core/RPChatPlugin.cs
@@ -0,0 +1,411 @@
+﻿using Rocket.API;
+using Rocket.API.Collections;
+using Rocket.API.Extensions;
+using Rocket.Core.Commands;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Events;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPChat.Core
+{
+    public class RPChatPlugin : RocketPlugin<RPChatConfig>
+    {
+        public static RPChatPlugin Instance;
+
+
+        protected override void Load()
+        {
+            Instance = this;
+            UnturnedPlayerEvents.OnPlayerChatted += onPlayerChatted;
+        }
+        protected override void Unload()
+        {
+            Instance = null;
+            UnturnedPlayerEvents.OnPlayerChatted -= onPlayerChatted;
+        }
+
+
+        public override TranslationList DefaultTranslations => new TranslationList()
+        {
+            { "global_chat_is_disabled",    "Глобальный чат отключен, используйте /nrp;" }, // -
+            { "local_chat_is_disabled",     "Локальный чат отключен!" },                    // -
+            { "group_chat_is_disabled",     "Групповой чат отключен!" },                    // -
+
+            { "command_nrp_message", "[NRP] {0}: {1}" },    // {0} - player name, {1} - message
+
+            { "command_sms_player_notfound",        "Игрок с таким именем не найден" },         // -
+            { "command_sms_to_myself",              "Нет смысла писать sms самому себе" },      // -
+            { "command_sms_syntax",                 "Используйте: /sms (игрок) [текст]" },      // -
+            { "command_sms_dont_have_item",         "Купите телефон чтобы отправлять SMS" },    // {0} - item id
+            { "command_sms_not_enough_currency",    "На вашем балансе недостаточно средств" },  // {0} - corrent currency, {1} - sms cost, {2} - how much is missing
+            { "command_sms_sended",                 "SMS отправлено!" },                        // {0} - to player name, {1} - message
+            { "command_sms_message",                "[SMS от {0}]: {1}" },                      // {0} - from player name, {1} - message
+
+            { "command_ad_syntax",              "Используйте: /ad [текст]" },                   // -
+            { "command_ad_dont_have_item",      "Купите телефон чтобы отправить объявление" },  // {0} - item id
+            { "command_ad_not_enough_currency", "На вашем балансе недостаточно средств" },      // {0} - corrent currency, {1} - ad cost, {2} - how much is missing
+            { "command_ad_message",             "[Объявление] {0}: {1}" },                      // {0} - player name, {1} - announcement
+
+            { "command_dn_syntax",              "Используйте: /dn [текст]" },                               // -
+            { "command_dn_dont_have_item",      "Купите телефон чтобы отправить объявление в DarkNet" },    // {0} - item id
+            { "command_dn_not_enough_currency", "На вашем балансе недостаточно средств" },                  // {0} - corrent currency, {1} - dn cost, {2} - how much is missing
+            { "command_dn_message",             "[DarkNet Объявление] {0}: {1}" },                          // {0} - player name, {1} - announcement
+            
+
+            { "command_me_message", "[Действие] {0}: {1}" },    // {0} - player name, {1} - message
+            { "command_s_message", "{0} Крикнул: {1}" },        // {0} - player name, {1} - message
+            { "command_w_message", "{0} Прошептал: {1}" },      //  {0} - player name, {1} - message
+            
+            { "command_try_successfully", "удачно" },               // -
+            { "command_try_unsuccessfully", "неудачно" },           // - 
+            { "command_try_message", "[Дейтвие] {0}: {1} ({2})" },  // {0} - player name, {1} - message, {2} - result 
+        };
+
+
+
+        public string IgnoreDisableGlobalChatPermission => "rpchat.ignore.disable.global.chat"; // Permission for ignore disable global chat
+        public string IgnoreDisableLocalChatPermission => "rpchat.ignore.disable.global.chat";  // Permission for ignore disable local chat
+        public string IgnoreDisableGroupChatPermission => "rpchat.ignore.disable.global.chat";  // Permission for ignore disable group chat
+
+
+        private void onPlayerChatted(UnturnedPlayer player, ref Color color, string message, EChatMode chatMode, ref bool cancel)
+        {
+            if (chatMode == EChatMode.GLOBAL && Configuration.Instance.DisableGlobalChat && player?.HasPermission(IgnoreDisableGlobalChatPermission) == false)
+            {
+                // TODO: Say! Translate: global_chat_is_disabled
+                cancel = true;
+            }
+            else if (chatMode == EChatMode.LOCAL && Configuration.Instance.DisableLocalChat && player?.HasPermission(IgnoreDisableLocalChatPermission) == false)
+            {
+                // TODO: Say! Translate: local_chat_is_disabled
+                cancel = true;
+            }
+            else if (chatMode == EChatMode.GROUP && Configuration.Instance.DisableGroupChat && player?.HasPermission(IgnoreDisableGroupChatPermission) == false)
+            {
+                // TODO: Say! Translate: group_chat_is_disabled
+                cancel = true;
+            }
+        }
+
+
+        public List<SteamPlayer> GetSteamPlayersInRadius(Vector3 point, float radius, Predicate<SteamPlayer> predicate = null)
+        {
+            List<SteamPlayer> players = new List<SteamPlayer>();
+            foreach (SteamPlayer player in Provider.clients)
+            {
+                if (Vector3.Distance(point, player.player.transform.position) <= (radius == 0 ? float.MaxValue : radius))
+                {
+                    if (predicate?.Invoke(player) == false)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        players.Add(player);
+                    }
+                }
+            }
+            return players;
+        }
+
+
+
+        #region Commands:
+
+
+        [RocketCommand("nrp", "", "/nrp [text]", AllowedCaller.Player)]
+        [RocketCommandPermission("rpchat.nrp")]
+        private void ExecuteNrpCommand(IRocketPlayer caller, string[] command) // /nrp (text: 0)
+        {
+            UnturnedPlayer player = (UnturnedPlayer)caller;
+            string text = command.GetParameterString(0);
+            
+            if (text == null)
+            {
+                return;
+            }
+
+            ChatManager.serverSendMessage(
+                text:       Instance.Translate("command_nrp_message", player.CharacterName, text),
+                color:      UnturnedChat.GetColorFromName(Configuration.Instance.NRPChatColor, Color.green),
+                fromPlayer: player.SteamPlayer(),
+                toPlayer:   null,
+                mode:       EChatMode.SAY,
+                iconURL:    null,
+                Configuration.Instance.NRPChatUseRichTextFormatting);
+
+        }
+
+
+
+        [RocketCommand("sms", "", "/sms (player) [text]", AllowedCaller.Player)]
+        [RocketCommandPermission("rpchat.sms")]
+        private void ExecuteSmsCommand(IRocketPlayer caller, string[] command) // /sms (player: 0) [text: 1]
+        {
+            UnturnedPlayer player = (UnturnedPlayer)caller;
+            UnturnedPlayer toPlayer = UnturnedPlayer.FromName(command.GetStringParameter(0));
+
+
+            if (toPlayer == null)
+            {
+                UnturnedChat.Say(player, Instance.Translate("command_sms_player_notfound"), Color.red);
+                return;
+            }
+            else if (toPlayer == player)
+            {
+                UnturnedChat.Say(player, Instance.Translate("command_sms_to_myself"), Color.red);
+                return;
+            }
+
+
+            if (Configuration.Instance.SMSChatItemId != 0 && player.Player.inventory.search(Configuration.Instance.SMSChatItemId, true, true).Count <= 0)
+            {
+                UnturnedChat.Say(player, Instance.Translate("command_sms_dont_have_item", Configuration.Instance.SMSChatItemId), Color.red);
+                return;
+            }
+
+            if (player.Experience < Configuration.Instance.SMSChatCost)
+            {
+                UnturnedChat.Say(player, Instance.Translate("command_sms_not_enough_currency", player.Experience, Configuration.Instance.SMSChatCost,
+                    Configuration.Instance.SMSChatCost - player.Experience), Color.red);
+                return;
+            }
+
+            string text = command.GetParameterString(1);
+            if (text == null)
+            {
+                UnturnedChat.Say(player, Instance.Translate("command_sms_syntax"), Color.yellow);
+                return;
+            }
+
+
+            player.Experience -= Configuration.Instance.SMSChatCost;
+
+
+            UnturnedChat.Say(player, Instance.Translate("command_sms_sended", toPlayer.CharacterName, text), UnturnedChat.GetColorFromName(Configuration.Instance.SMSChatColor, Color.green));
+
+
+            ChatManager.serverSendMessage(
+                text:       Instance.Translate("command_sms_message", player.CharacterName, text),
+                color:      UnturnedChat.GetColorFromName(Configuration.Instance.SMSChatColor, Color.green),
+                fromPlayer: player.SteamPlayer(),
+                toPlayer:   toPlayer.SteamPlayer(),
+                mode:       EChatMode.SAY,
+                iconURL:    string.IsNullOrEmpty(Configuration.Instance.SMSChatIcon) ? null : Configuration.Instance.SMSChatIcon,
+                Configuration.Instance.SMSChatUseRichTextFormating);
+        }
+
+
+
+        public string ADChatLintenerPermission => "rpchat.ad.listener";
+        [RocketCommand("ad", "", "/ad [text]", AllowedCaller.Player)]
+        [RocketCommandPermission("rpchat.ad")]
+        private void ExecuteAdCommand(IRocketPlayer caller, string[] command) // /ad (text: 0)
+        {
+            UnturnedPlayer player = (UnturnedPlayer)caller;
+
+            if (Configuration.Instance.ADChatItemId != 0 && player.Player.inventory.search(Configuration.Instance.ADChatItemId, true, true).Count <= 0)
+            {
+                UnturnedChat.Say(player, Instance.Translate("command_ad_dont_have_item", Configuration.Instance.ADChatItemId), Color.red);
+                return;
+            }
+
+            if (player.Experience < Configuration.Instance.ADChatCost)
+            {
+                UnturnedChat.Say(player, Instance.Translate("command_ad_not_enough_currency", player.Experience, Configuration.Instance.ADChatCost,
+                    Configuration.Instance.ADChatCost - player.Experience), Color.red);
+                return;
+            }
+
+            string text = command.GetParameterString(0);
+            if (text == null)
+            {
+                UnturnedChat.Say(player, Instance.Translate("command_ad_syntax"), Color.yellow);
+                return;
+            }
+
+
+            player.Experience -= Configuration.Instance.ADChatCost;
+
+
+            GetSteamPlayersInRadius(player.Position, Configuration.Instance.ADChatRadius, (p) => UnturnedPlayer.FromSteamPlayer(p).HasPermission(ADChatLintenerPermission)).ForEach((steamPlayer) =>
+            {
+                ChatManager.serverSendMessage(
+                    text: Instance.Translate("command_ad_message", player.CharacterName, text),
+                    color: UnturnedChat.GetColorFromName(Configuration.Instance.ADChatColor, Color.green),
+                    fromPlayer: player.SteamPlayer(),
+                    toPlayer: steamPlayer,
+                    mode: EChatMode.SAY,
+                    iconURL: string.IsNullOrEmpty(Configuration.Instance.ADChatIcon) ? null : Configuration.Instance.ADChatIcon,
+                    Configuration.Instance.ADChatUseRichTextFormating);
+            });
+        }
+
+
+
+        public string DNChatListenerPermission => "rpchat.dn.listener";
+        [RocketCommand("dn", "", "/dn [text]", AllowedCaller.Player)]
+        [RocketCommandPermission("rpchat.dn")]
+        private void ExecuteDnCommand(IRocketPlayer caller, string[] command) // /dn (text: 0)
+        {
+            UnturnedPlayer player = (UnturnedPlayer)caller;
+
+            if (Configuration.Instance.DNChatItemId != 0 && player.Player.inventory.search(Configuration.Instance.DNChatItemId, true, true).Count <= 0)
+            {
+                UnturnedChat.Say(player, Instance.Translate("command_dn_dont_have_item", Configuration.Instance.DNChatItemId), Color.red);
+                return;
+            }
+
+            if (player.Experience < Configuration.Instance.DNChatCost)
+            {
+                UnturnedChat.Say(player, Instance.Translate("command_dn_not_enough_currency", player.Experience, Configuration.Instance.DNChatCost,
+                    Configuration.Instance.ADChatCost - player.Experience), Color.red);
+                return;
+            }
+
+            string text = command.GetParameterString(0);
+            if (text == null)
+            {
+                UnturnedChat.Say(player, Instance.Translate("command_dn_syntax"), Color.yellow);
+                return;
+            }
+
+
+            player.Experience -= Configuration.Instance.DNChatCost;
+
+
+            GetSteamPlayersInRadius(player.Position, Configuration.Instance.DNChatRadius, (p) => UnturnedPlayer.FromSteamPlayer(p).HasPermission(DNChatListenerPermission)).ForEach((steamPlayer) => 
+            {
+                ChatManager.serverSendMessage(
+                    text: Instance.Translate("command_dn_message", player.CharacterName, text),
+                    color: UnturnedChat.GetColorFromName(Configuration.Instance.DNChatColor, Color.green),
+                    fromPlayer: player.SteamPlayer(),
+                    toPlayer: steamPlayer,
+                    mode: EChatMode.SAY,
+                    iconURL: string.IsNullOrEmpty(Configuration.Instance.DNChatIcon) ? null : Configuration.Instance.DNChatIcon,
+                    Configuration.Instance.DNChatUseRichTextFormating);
+            });
+        }
+
+
+
+        [RocketCommand("me", "", "/me [text]", AllowedCaller.Player)]
+        [RocketCommandPermission("rpchat.me")]
+        private void ExecuteMeCommand(IRocketPlayer caller, string[] command) // /me (text: 0)
+        {
+            UnturnedPlayer player = (UnturnedPlayer)caller;
+            string text = command.GetParameterString(0);
+
+            if (text == null)
+            {
+                return;
+            }
+
+            GetSteamPlayersInRadius(player.Position, Configuration.Instance.MEChatRadius).ForEach((steamPlayer) => 
+            {
+                ChatManager.serverSendMessage(
+                    text:       Instance.Translate("command_me_message", player.CharacterName, text),
+                    color:      UnturnedChat.GetColorFromName(Instance.Configuration.Instance.MEChatColor, Color.green),
+                    fromPlayer: player.SteamPlayer(),
+                    toPlayer:   steamPlayer,
+                    mode:       EChatMode.SAY,
+                    iconURL:    null,
+                    Configuration.Instance.MEChatUseRichTextFormating);
+            });
+            
+            
+        }
+
+
+
+        [RocketCommand("s", "", "/s [text]", AllowedCaller.Player)]
+        [RocketCommandPermission("rpchat.s")]
+        private void ExecuteSCommand(IRocketPlayer caller, string[] command) // /s (text: 0)
+        {
+            UnturnedPlayer player = (UnturnedPlayer)caller;
+            string text = command.GetParameterString(0);
+
+            if (text == null)
+            {
+                return;
+            }
+
+            GetSteamPlayersInRadius(player.Position, Configuration.Instance.SChatRadius).ForEach((steamPlayer) =>
+            {
+                ChatManager.serverSendMessage(
+                    text:       Instance.Translate("command_s_message", player.CharacterName, text),
+                    color:      UnturnedChat.GetColorFromName(Configuration.Instance.SChatColor, Color.green),
+                    fromPlayer: player.SteamPlayer(),
+                    toPlayer:   steamPlayer,
+                    mode:       EChatMode.SAY,
+                    iconURL:    null,
+                    Configuration.Instance.SChatUseRichTextFormating);
+            });
+        }
+
+
+
+        [RocketCommand("w", "", "/w [text]", AllowedCaller.Player)]
+        [RocketCommandPermission("rpchat.w")]
+        private void ExecuteWCommand(IRocketPlayer caller, string[] command) // /w (text: 0)
+        {
+            UnturnedPlayer player = (UnturnedPlayer)caller;
+            string text = command.GetParameterString(0);
+
+            if (text == null)
+            {
+                return;
+            }
+
+            GetSteamPlayersInRadius(player.Position, Configuration.Instance.WChatRadius).ForEach((steamPlayer) => 
+            {
+                ChatManager.serverSendMessage(
+                    text:       Instance.Translate("command_w_message", player.CharacterName, text),
+                    color:      UnturnedChat.GetColorFromName(Configuration.Instance.WChatColor, Color.green),
+                    fromPlayer: player.SteamPlayer(),
+                    toPlayer:   steamPlayer,
+                    mode:       EChatMode.SAY,
+                    iconURL:    null,
+                    Configuration.Instance.WChatUseRichTextFormating);
+            });
+        }
+
+
+
+        [RocketCommand("try", "", "/try [text]", AllowedCaller.Player)]
+        [RocketCommandPermission("rpchat.try")]
+        private void ExecuteTryCommand(IRocketPlayer caller, string[] command) // /try (text: 0)
+        {
+            UnturnedPlayer player = (UnturnedPlayer)caller;
+            string text = command.GetParameterString(0);
+
+            if (text == null)
+            {
+                return;
+            }
+
+            string res = UnityEngine.Random.Range(0f, 1f) > 0.5f ? Instance.Translate("command_try_successfully") : Instance.Translate("command_try_unsuccessfully");
+
+            GetSteamPlayersInRadius(player.Position, Configuration.Instance.TRYChatRadius).ForEach((steamPlayer) =>
+            {
+                ChatManager.serverSendMessage(
+                    text:       Instance.Translate("command_try_message", player.CharacterName, text, res),
+                    color:      UnturnedChat.GetColorFromName(Configuration.Instance.TRYChatColor, Color.green),
+                    fromPlayer: player.SteamPlayer(),
+                    toPlayer:   steamPlayer,
+                    mode:       EChatMode.SAY,
+                    iconURL:    null,
+                    Configuration.Instance.TRYChatUseRichTextFormating);
+            });
+        }
+
+
+
+        #endregion
+    }
+}
